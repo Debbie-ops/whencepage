4afce9b120443d402cd955e0593a3891426e5532
Daliso <miti26miti@gmail.com>
2024-01-29 11:45:28 -0800

    first commit

---
+++ Branches.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>whencegroup</title>
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<a href="index.html" class="back-to-home">
+  <i class="bx bx-arrow-back" style="font-size: 24px;"></i> 
+</a>
+<!-- ======= Services Section ======= -->
+<section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Branches</h2>
+          <p style="font-size: 25px;">Discover the convenience of our widespread presence with branches across all 10 provinces of Zambia, 
+            making it easier for you to visit and explore the opportunities that our loan services offer</p>
+        </div>
+
+    
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box" >
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">ZIMCO HQ</a></h4>
+              <p>Zimco House Building<br>
+                Suites 401 & 402 4th Floor Chainda Place<br>
+                (+26) 0971 089407<br>
+                info@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">KAMBENDEKELA</a></h4>
+              <p>Kambendekela House Building<br>
+                Suites F13 & F14 <br>
+                (+26) 0978 966347<br>
+                Kambendekela@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">ANCHOR HSE</a></h4>
+              <p>Anchor House<br>
+                Cairo Road, Lusaka<br>
+                (+26) 0972 274147<br>
+                anchor@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">KABWE</a></h4>
+              <p>
+                Mukuni House, 3rd Floor, Suite 382<br>
+                (+26) 0962 920883<br>
+                kabwe@whencegroup.com
+                </p>>
+            </div>
+          </div>
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">KITWE</a></h4>
+              <p>Post Office Building<br>
+                Matuka Avenue, Suites 205, 3rd Floor <br>
+                (+26) 0770 898120<br>
+                kitwe@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">NDOLA</a></h4>
+              <p>Compensation House<br>
+                Corner of Broadway/Moffat Road<br>
+                Ground Floor Room 30<br>
+                
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">SOLWEZI</a></h4>
+              <p>2nd Floor Independence Road<br>
+                Suites 38 & 39<br>
+                (+26) 0973108416<br>
+                solwezi@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">MANSA</a></h4>
+              <p>Napsa Building<br>
+                Corner of Chember and Chitimukulu Road <br>
+                (+26) 0974867727<br>
+                mansa@whencegroup.com
+                </p>
+            </div>
+          </div>
+        </div>
+          
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">KASAMA</a></h4>
+              <p>Compensation House<br>
+                Office Zambia Road Kasama<br>
+                Ground Floor Office 07 and 19/20 <br>
+                (+26) 0974845202<br>
+                kasama@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">CHIPATA</a></h4>
+              <p>NAPSA Building<br>
+                Ground Floor
+                Suite LG8 & LG9 Lower<br>
+                (+26) 0761910760<br>
+                chipata@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">LIVINGSTONE</a></h4>
+              <p>Mosi Oa Tunya House New Wing<br>
+                5th Floor room 505 & 3rd Floor Room 315<br>
+                (+26) 0972 489589<br>
+                lstone@whencegroup.com
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">MAZABUKA</a></h4>
+              <p>Galaba House
+                Rooms 00 & 03<br>
+                
+                </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">KAFUE</a></h4>
+              <p>Kafue House
+                Rooms 08 & 16<br>
+                
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">CHINGOLA</a></h4>
+              <p>Compensation House
+                Town Center<br>
+                Ground Floor 10698<br>
+                (+26) 0763024487
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">CHOMA</a></h4>
+              <p>Butala House
+                Room 106 & 108<br>
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">PETAUKE</a></h4>
+              <p>Adelaide Complex along Boma Road<br>
+                (Opposite Petauke fire station)<br>
+                
+                </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">KALUMBILA</a></h4>
+              <p>Banana 2, West Road T26/479<br>
+                </p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxs-business"></i></div>
+              <h4><a href="">MONGU</a></h4>
+              <p>Napsa Building
+                Room 418 and 417 <br>
+                (+26) 0975845112<br>
+                mongu@whencegroup.com
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    
+    <!-- End Services Section -->
+
+  <!-- ======= Clients Section ======= --
+
+    <section id="clients" class="clients">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="clients-slider swiper">
+          <div class="swiper-wrapper align-items-center">
+            <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+</body>
+
+</html>
